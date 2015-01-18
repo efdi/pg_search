967548ffb1d56481ede8bd1052cb07fc4ef8fe1d
Tanner Welsh <tannerwelsh@gmail.com>
2015-01-18 00:17:42 -0500

    Refactor TSQueryable#tsquery_for_term into smaller private methods

--- lib/pg_search/features/tsearch/ts_queryable.rb
+++ lib/pg_search/features/tsearch/ts_queryable.rb
@@ -6,40 +6,68 @@ module PgSearch
 
         def tsquery
           return "''" if query.blank?
+
           query_terms = query.split(" ").compact
-          tsquery_terms = query_terms.map { |term| tsquery_for_term(term) }
-          tsquery_terms.join(options[:any_word] ? ' || ' : ' && ')
+
+          tsquery_terms(query_terms).join(options[:any_word] ? ' || ' : ' && ')
         end
 
-        def tsquery_for_term(unsanitized_term)
-          if options[:negation] && unsanitized_term.start_with?("!")
-            unsanitized_term[0] = ''
-            negated = true
+        private
+
+        def tsquery_terms(query_terms)
+          query_terms.map do |term|
+            unsanitized_term, negated = check_for_negation(term)
+            sanitized_term = sanitize(unsanitized_term)
+
+            terms = make_terms( term_sql(sanitized_term), negated )
+
+            build_sql( tsquery_sql(terms) )
           end
+        end
 
-          sanitized_term = unsanitized_term.gsub(DISALLOWED_TSQUERY_CHARACTERS, " ")
+        def build_sql(tsquery_sql)
+          Arel::Nodes::NamedFunction.new(
+            "to_tsquery",
+            [dictionary, tsquery_sql]
+          ).to_sql
+        end
 
-          term_sql = Arel.sql(normalize(connection.quote(sanitized_term)))
+        def tsquery_sql(terms)
+          terms.inject do |memo, term|
+            Arel::Nodes::InfixOperation.new("||", memo, Compatibility.build_quoted(term))
+          end
+        end
 
+        def make_terms(term_sql, negated)
           # After this, the SQL expression evaluates to a string containing the term surrounded by single-quotes.
           # If :prefix is true, then the term will have :* appended to the end.
-          # If :negated is true, then the term will have ! prepended to the front.
-          terms = [
+          # If negated is true, then the term will have ! prepended to the front.
+          [
             (Compatibility.build_quoted('!') if negated),
             Compatibility.build_quoted("' "),
             term_sql,
             Compatibility.build_quoted(" '"),
             (Compatibility.build_quoted(":*") if options[:prefix])
           ].compact
+        end
 
-          tsquery_sql = terms.inject do |memo, term|
-            Arel::Nodes::InfixOperation.new("||", memo, Compatibility.build_quoted(term))
+        def term_sql(term)
+          Arel.sql(normalize(connection.quote(term)))
+        end
+
+        def check_for_negation(term)
+          if options[:negation] && term.start_with?("!")
+            term[0] = ''
+            negated = true
+          else
+            negated = false
           end
 
-          Arel::Nodes::NamedFunction.new(
-            "to_tsquery",
-            [dictionary, tsquery_sql]
-          ).to_sql
+          [term, negated]
+        end
+
+        def sanitize(term)
+          term.gsub(DISALLOWED_TSQUERY_CHARACTERS, " ")
         end
       end
     end
